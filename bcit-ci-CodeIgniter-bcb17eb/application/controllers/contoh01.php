ee3d55c196336fb14f17bac3ab74c16080fe6264
Dedenzhulfa <dedenzm01@gmail.com>
2023-09-26 13:33:21 +0700

    Membuat contoh01.php

---
+++ bcit-ci-CodeIgniter-bcb17eb/application/controllers/contoh01.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh01 extends CI_Controller
+{
+    public function index()
+    {
+        echo"<h1>Perkenalan</h1>";
+        echo"Nama saya Deden Jhulfa Mustafha
+            Saya tinggal di Babelan
+            olahraga yang saya sukai adalah Beladiri dan Bulutangkis";
+    }
+}
