d401052b88a66b1ec55ba7d9f85d68742ea0d66b
Dedenzhulfa <dedenzm01@gmail.com>
2023-10-02 17:16:59 +0700

    Mengerjakan contoh latihan 2

--- application/controllers/latihan1.php
+++ application/controllers/latihan1.php
@@ -4,6 +4,13 @@ class latihan1 extends CI_Controller
     public function index()
     {
         echo "Selamat datang... selamat belajar Web Programming";
-        //this->load->view('view latihan')
-}
+        //this->load->view('view-latihan1');
+    }
+    
+    public function penjumlahan($n1, $n2)
+    {
+        $this->load->model('Model_latihan1');
+        $hasil = $this->Model_latihan1->jumlah($n1, $n2);
+        echo "Hasil penjumlahan dari". $n1 . " + ". $n2 ." = ". $hasil;
+    }
 }
