88d250f315aecb5446e8d84a7d29ca31427f2e48
Dedenzhulfa <dedenzm01@gmail.com>
2023-10-02 17:37:13 +0700

    Membuat latihan 1

---
+++ application/controllers/latihan1.php
@@ -0,0 +1,9 @@
+<?php
+class latihan1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "Selamat datang... selamat belajar Web Programming";
+        //this->load->view('view latihan')
+}
+}
